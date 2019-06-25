9abd395f312c5d267566460d3b6197e689718334
Bruno Ferreira <brunoferreira.ebm@gmail.com>
2019-06-25 17:13:24 +0100

    fim do exercicio condicionais

---
+++ src/pt/com/everis/academia/java/Condicionais.java
@@ -0,0 +1,22 @@
+package pt.com.everis.academia.java;
+
+public class Condicionais {
+
+	public static void main(String[] args) {
+		 
+		int value1 = 1;
+		int value2 = 2;
+		
+		if(value1 == 1 && value2 == 2) {
+			System.out.println("value1 is 1 AND value2 is 2");
+		}
+		
+		if(value1 == 1 || value2 == 2) {
+			System.out.println("value1 is 1 OR value2 is 2");
+		}
+		
+		boolean someCondition = (value1 == 1 && value2 == 2) ? true : false;	
+		System.out.println(someCondition);
+	}
+
+}
