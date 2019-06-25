62ced920864c13c5c21ec492f35cf1bcfab5259b
Bruno Ferreira <brunoferreira.ebm@gmail.com>
2019-06-25 15:48:46 +0100

    fim do exercicio equals

--- src/pt/com/everis/academia/java/Book.java
+++ src/pt/com/everis/academia/java/Book.java
@@ -5,9 +5,31 @@ public class Book {
 	private String isbn;
 
 	public Book(String isbn) {
+		super();
 		this.isbn = isbn;
 	}
 	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		return true;
+	}
+
 	public static void main(String[] args) {
 		Book book1 = new Book("1234");
 		Book book2 = new Book("1234");
