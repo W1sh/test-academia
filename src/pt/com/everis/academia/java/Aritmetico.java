98a674d0b406b2453b91dcb2e87d03e8e322988d
Bruno Ferreira <brunoferreira.ebm@gmail.com>
2019-06-25 16:56:32 +0100

    fim do exercicio aritmetico

---
+++ src/pt/com/everis/academia/java/Aritmetico.java
@@ -0,0 +1,26 @@
+package pt.com.everis.academia.java;
+
+public class Aritmetico {
+
+	public static void main(String[] args) {
+		
+		int result = 1 + 2;
+		System.out.println(result);
+		
+		result = result - 1;
+		System.out.println(result);
+		
+		result = result * 2;
+		System.out.println(result);
+		
+		result = result / 2;
+		System.out.println(result);
+		
+		result = result + 8;
+		System.out.println(result);
+		
+		result = result % 7;
+		System.out.println(result);
+	}
+
+}
