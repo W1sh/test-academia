c46cb150eed9da2eee78636b70c64e7e958fe4f0
Bruno Ferreira <brunoferreira.ebm@gmail.com>
2019-06-25 13:56:26 +0100

    fim do exercicio boxing

--- src/pt/com/everis/academia/java/ValueOf.java
+++ src/pt/com/everis/academia/java/ValueOf.java
@@ -5,5 +5,17 @@ public class ValueOf {
 	public static void main(String[] args) {
 		System.out.println(Boolean.valueOf(true));
 		System.out.println(Boolean.valueOf("true"));
+		
+		// ------------ Boxing/Autoboxing ------------
+		
+		Integer i = new Integer(125);
+		Integer j = i;
+		System.out.println(i==j);
+		i++;
+		System.out.println("i = " + i);
+		System.out.println(i==j);
+		j++;
+		System.out.println(i==j);
+		System.out.println(i.equals(j));
 	}
 }
