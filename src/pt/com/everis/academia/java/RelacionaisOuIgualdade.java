8a87bf669d34af6b7e8c12aa0c10358eef7f53cf
Bruno Ferreira <brunoferreira.ebm@gmail.com>
2019-06-25 17:03:25 +0100

    fim do exercicio relacionais ou igualdade

---
+++ src/pt/com/everis/academia/java/RelacionaisOuIgualdade.java
@@ -0,0 +1,30 @@
+package pt.com.everis.academia.java;
+
+public class RelacionaisOuIgualdade {
+
+	public static void main(String[] args) {
+		int value1 = 1;
+		int value2 = 2;
+		
+		if(value1 == value2) {
+			System.out.println(false);
+		}
+		
+		if(value1 != value2) {
+			System.out.println(true);
+		}
+		
+		if(value1 > value2) {
+			System.out.println(false);
+		}
+		
+		if(value1 < value2) {
+			System.out.println(true);
+		}
+		
+		if(value1 <= value2) {
+			System.out.println(true);
+		}
+	}
+
+}
