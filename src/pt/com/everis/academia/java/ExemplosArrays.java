9d1d0f33e15f91ae552963c26a6d36c046bc0aa0
Bruno Ferreira <brunoferreira.ebm@gmail.com>
2019-06-26 11:27:30 +0100

    exercicio sort pais

--- src/pt/com/everis/academia/java/ExemplosArrays.java
+++ src/pt/com/everis/academia/java/ExemplosArrays.java
@@ -7,11 +7,18 @@ public class ExemplosArrays {
 	public static void main(String[] args) {
 		int[] array = new int[5];
 		array[0] = 1;
-		array[1] = 2;
-		array[2] = 3;
-		array[3] = 4;
-		array[4] = 5;
+		array[1] = 4;
+		array[2] = 5;
+		array[3] = 2;
+		array[4] = 3;
+		
+		Arrays.sort(array);
+		
+		int[] newArray = Arrays.copyOfRange(array, 1, 3);
+		
 		System.out.println(array.toString());
 		System.out.println(Arrays.toString(array));
+	
+		System.out.println(Arrays.toString(newArray));
 	}
 }
--- src/pt/com/everis/academia/java/Pai.java
+++ src/pt/com/everis/academia/java/Pai.java
@@ -1,8 +1,47 @@
 package pt.com.everis.academia.java;
 
-public class Pai {
+import java.util.Arrays;
 
+public class Pai implements Comparable<Pai>{
+	
+	private Integer id;
+	
 	public Pai() {
-		// TODO Auto-generated constructor stub
+		super();
+	}
+
+	public Pai(Integer id) {
+		super();
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public static void main(String[] args) {
+		Pai[] pais = new Pai[5];
+		pais[0] = new Pai(2);
+		pais[1] = new Pai(1);
+		pais[2] = new Pai(4);
+		pais[3] = new Pai(5);
+		pais[4] = new Pai(3);
+		
+		Arrays.sort(pais);
+		System.out.println(Arrays.toString(pais));
+	}
+
+	@Override
+	public int compareTo(Pai o) {
+		return this.id - o.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Pai [id=" + id + "]";
 	}
 }
